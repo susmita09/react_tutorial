bd984f82e7ae15647f264e7698e0ec9a0da0ec6d
susmita <susmitamukherjee165@gmail.com>
2023-01-06 02:01:39 +0530

    assignment_2

---
+++ app.js
@@ -0,0 +1,15 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
+// Parcel d all these work for us
+// Bundling
+//Minify
+//cleaning our code
+//dev and production build
+//Image optimization
+//https on dev
+
+
+const Heading = React.cloneElement("h1", {
+    id : "title"
+}, "heading 1 from parcel");
